224b658bbc54c994f3d7ec23c1fba94901f4f3c4
rybkin <theimdim@gmail.com>
2018-12-12 20:47:02 +0400

    Добавил файлы сборки

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import html from "./template/pages/index.pug"
+// Import styles
+import style from "./styles/main.scss";
+// Import scripts
+import script from "./scripts/main.js";
+console.log('all files loaded');
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+
+      {
+        test: /\.pug$/,
+        use: [
+          "html-loader",
+          "pug-html-loader"
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            MiniCssExtractPlugin.loader,
+            // "style-loader", // style nodes from js strings
+            "css-loader",
+            "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/template/pages/index.pug",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new CopyWebpackPlugin([
+      { from: './src/images', to: './images' },
+      { from: './src/fonts', to: './fonts' },
+    ])
+  ]
+};
